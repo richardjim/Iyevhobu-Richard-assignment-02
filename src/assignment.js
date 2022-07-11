74c116d9098c9a35ce356557004cc1626210accf
Richard Iyevhobu <79172294+richardjim@users.noreply.github.com>
2022-07-11 20:38:29 +0000

    check challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -18,7 +18,7 @@ function sumOfNumbers(arrayOfNumbers) {
   let sum = 0;
   for(let i=0; i < arrayOfNumbers.length; i++){
     sum+=arrayOfNumbers[i];
-  }
+  };
     return sum;
   
 };
@@ -34,9 +34,15 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+   let even = 0;
+  for(let i=0; i < arrayOfNumbers.length; i++){
+    if( arrayOfNumbers[i]% 2 === 0)
+     even++;
+  };
+    return even;
+
+};
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
